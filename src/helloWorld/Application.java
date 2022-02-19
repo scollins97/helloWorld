a75bf2a9120a49e64b2daa8ef38a1f41c891cfff
scollins97 <srcollins575@gmail.com>
2022-02-19 18:00:59 -0500

    second commit

--- src/helloWorld/Application.java
+++ src/helloWorld/Application.java
@@ -4,8 +4,11 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
-		
-		
+	
+		System.out.println("Hello World");
+		System.out.print("Hello");
+		System.out.print("it's me");
+	
 	}
 	
 	
